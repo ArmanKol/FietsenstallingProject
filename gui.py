a046785032c38c5c5478c8b7f27ccd8547c71cc8
Arman.K <arman.koldaguc@student.hu.nl>
2017-10-23 13:30:03 +0200

    ArmanKoldaguc

--- gui.py
+++ gui.py
@@ -1,15 +1,17 @@
 import tkinter
 
 root = tkinter.Tk()
-nslogo = tkinter.PhotoImage(file="C:\\Users\\Arman.K\\PycharmProjects\\FietsenstallingProject\\NSLogo.png", width=40, height=40)
+#nslogo = tkinter.PhotoImage(file="C:\\Users\\Arman.K\\PycharmProjects\\FietsenstallingProject\\NSLogo.png", width=40, height=40)
 
 root.title("NS-Fietsenstalling")
 
-achtergrond_label = tkinter.Label(master=root, image=nslogo)
-achtergrond_label.pack()
+#achtergrond_label = tkinter.Label(master=root, image=nslogo)
+#achtergrond_label.pack()
 
 registrerenknop = tkinter.Button(master=root, text="Fiets registreren")
 registrerenknop.pack()
+
+
 
 ophalenKnop = tkinter.Button(master=root, text="Fiets ophalen")
 ophalenKnop.pack()
