423060ed84ce086e36379a12bb519726e0a3126b
Lamski <liam.mulder@student.hu.nl>
2017-10-23 14:50:54 +0200

    Bestanden aangemaakt

--- Fietsenstalling.py
+++ Fietsenstalling.py
@@ -1,11 +1,46 @@
-def inloggen():
-    e_mail = str(input("Geef je e-mailadres: "))
-    wachtwoord = str(input("Geef je wachtwoord: "))
+import csv
 
-    bestand = open("database/gebruikers.csv", "r")
-    lines = bestand.readlines()
-    bestand.close()
-    for regels in lines:
-        print(regels)
+def csvread():
+    with open("database/gebruikers.csv", "r") as ReadMyCsv:
+        reader = csv.DictReader(ReadMyCsv, delimiter=";")
+
+        gegevens = []
+        for gegeven in reader:
+            gegevens.append(gegeven)
+
+    return gegevens
+
+
+def inloggen():
+    correctLogin = 0
+    pogingen = 5
+    gegevens = csvread()
+    while correctLogin != 1:
+        if pogingen == 0:
+            break
+        mail = str(input("Geef je e-mailadres: "))
+        wachtwoord = str(input("Geef je wachtwoord: "))
+        lengte_lijst = 0
+        for gegeven in gegevens:
+            if mail == gegeven["mail"]:
+                if wachtwoord == "":
+                    print("Er is geen wachtwoord ingevoerd.")
+                    break
+                if wachtwoord == gegeven["wachtwoord"]:
+                    print("Combinatie klopt.")
+                    correctLogin = 1
+                    break
+                else:
+                    print("De combinatie is niet goed.")
+                    pogingen -= 1
+                    if pogingen != 0:
+                        print("U heeft nog " + str(pogingen) + " pogingen over.")
+            else:
+                if lengte_lijst == (len(gegevens)-1):
+                    print("Het e-mailadres is niet gevonden.")
+                lengte_lijst += 1
+                pass
+    if pogingen == 0:
+        print("Uw heeft te vaak geprobeert in te loggen.")
 
 inloggen()
