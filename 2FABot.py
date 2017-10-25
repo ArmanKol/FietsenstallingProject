3951f5ec0da0ee8efb43d2f073162d5808fd1a99
Unknown <g.manschot@gmail.com>
2017-10-25 02:23:20 +0200

    woah zeg
    moh

--- 2FABot.py
+++ 2FABot.py
@@ -1,33 +1,27 @@
 import telepot
 import pyotp #Importeert de Telegram & 2FA API's
 from telepot.loop import MessageLoop
+import csv
 from pprint import pprint #OwO mooie tekst
+from Fietsenstalling import csvread
 
-bot = telepot.Bot("370325529:AAGKGqP-dHRoyKb2FKnPtMyYCdOhcGKLK5Q")
+####2FA- & Botgedeelte
+bot = telepot.Bot("370325529:AAGKGqP-dHRoyKb2FKnPtMyYCdOhcGKLK5Q") #Hash voor de bot
 response = bot.getUpdates() #pakt het laatst verzonden bericht aan de bot
-response_1 = response[1]
+response_1 = response[-1]
 UserID = response_1['message']['chat']['id'] #pakt het ID van de verzender
+UserBericht = response_1['message']['text']
 
-FietsID = ####Vogel uit hoe je enkel het bericht leest, dit moet overeenkomen met de .csv
+def TFACode():
+    print("bla")
+def BotBericht():
+    gebruiker_gegevens = csvread("gebruikers.csv")
+    print("Stuur een bericht met je Fietsnummer naar https://t.me/BevFietsBot")
+    for gebruiker_gegeven in gebruiker_gegevens:
+        while True:
+            if UserBericht == gebruiker_gegeven["fietsnummer"]:
+                bot.sendMessage(UserID, 'Placeholder voor Pyotp.TOTP')
+                break
+            else: bot.sendMessage(UserID, "Dit is niet je fietscode, probeer opnieuw.")
 
-
-
-def TwoFactor(TFAcode):
-    while True:
-        if FietsID == :####whatever er in de .csv staat
-            TFAcode = ####Hier nog een pyotp Time-based OTP invoeren
-            bot.sendMessage(UserID, 'Placeholder voor Pyotp.TOTP')
-        else: bot.sendMessage(UserID, 'Dat is niet het ID van je fiets')
-        return TFAcode
-
-def UnlockFiets():
-    while True:
-        if input('Voer hier je TFACode in om je fiets op te kunnen halen: ') == TFACode: ##komt nog wel
-            print('Je kan bij je fiets!')
-        else: input('Dit klopt niet. Probeer opnieuw: ')
-
-TwoFactor()
-UnlockFiets()
-
-
-####klopt hier het een en ander niet I know, maar komt nog wel
+BotBericht()
